75612e2ead33f124783e0cbacfc69213632b6b4f
archaprajapati2905@gmail.com <archaprajapati2905@gmail.com>
2023-09-30 15:14:00 +0530

    30-9-23

---
+++ pyramid.py
@@ -0,0 +1,29 @@
+##11
+##for i in range(0,5):
+##    for k in range(5,i,-1):
+##        print(" ",end=" ")
+##    for l in range(0,2*i+1):
+##        print("*",end=" ")
+##    for m in range(5,i,-1):
+##        print(" ",end=" ")
+##    print("\n")
+##
+##    
+##        
+##22            
+##for i in range(0,10):
+##    for j in range(0,i):
+##        print("*",end=" ")
+##    print("\n")
+##
+##
+##33
+
+for i in range(0,10):
+    for j in range(10,i,-1):
+        print(" ",end=" ")
+    for k in range(0,i+1):
+        print("*",end=" ")
+    print("\n")
+
+
